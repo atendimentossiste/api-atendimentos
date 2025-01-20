897ace5595db1f25d3ebb6f854be9d88f34be617
cotete <feliperibeirots@gmail.com>
2025-01-20 09:55:28 -0300

    tentativa com o postgre

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.0].define(version: 2025_01_18_145812) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
+
   create_table "chat_clientes", force: :cascade do |t|
     t.string "nm_cliente"
     t.string "nr_cliente"
